d2bebd99049eca9ac7c549c110732ede75bb67ef
Orlando Buzana <orlandobuzana@outlook.com>
2019-07-24 03:20:57 -0400

    Added bootstrap and finised diplay gif loop

--- assets/script/index.js
+++ assets/script/index.js
@@ -5,7 +5,7 @@ $("#searchBtn").on('click',function(){
     //att value of the intext to a var 
     let searchEntry = $("#searchBox").val();
     //API url + var as a seach key
-    let queryURL ="https://api.giphy.com/v1/gifs/search?api_key=PXAaADQHCfE50KlR8lkiTjmzTKxKH0Db&q="+searchEntry+"&limit=40&offset=0&rating=G&lang=en";
+    let queryURL ="https://api.giphy.com/v1/gifs/search?api_key=PXAaADQHCfE50KlR8lkiTjmzTKxKH0Db&q="+searchEntry+"&limit=10&offset=0&rating=G&lang=en";
     console.log("this"+queryURL);
     //API call 
     $.ajax({
@@ -13,7 +13,7 @@ $("#searchBtn").on('click',function(){
         method: "GET"
       }).done(function(response) {
           //for loop to add gifs to the display 
-          for (var i = 0;i<40;i++){
+          for (var i = 0;i<10;i++){
               console.log(response.data[i]);
               $('#gifs').append("<img alt ='"+response.data[i].title+"'src='"+response.data[i].images.fixed_height_downsampled.url+"'>");
             }
